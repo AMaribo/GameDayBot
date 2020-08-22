4ba8763457e01ea6c2f85d5756598270e6387d07
AMaribo <alexander.m.mackenzie@gmail.com>
2020-08-22 13:39:53 -0500

    Completed shift from discord.io to discord.js; bot works again

--- bot.js
+++ bot.js
@@ -1,20 +1,9 @@
-const Discord = require('discord.io');
+const Discord = require('discord.js');
 const fs = require('fs');
-const logger = require('winston');
 const { prefix, token } = require('./config.json');
 
-// Configure logger settings
-logger.remove(logger.transports.Console);
-logger.add(new logger.transports.Console, {
-    colorize: true
-});
-logger.level = 'debug';
-
 // Initialize Discord Bot
-var bot = new Discord.Client({
-   token: token,
-   autorun: true
-});
+var bot = new Discord.Client();
 
 bot.commands = new Discord.Collection();
 
@@ -27,17 +16,17 @@ for (const file of commandFiles) {
 
 const cooldowns = new Discord.Collection();
 
-bot.on('ready', function (evt) {
-    logger.info('Connected');
-    logger.info('Logged in as: ');
-    logger.info(bot.username + ' - (' + bot.id + ')');
-});
+bot.once('ready', () => {
+    console.log('Ready!');
+})
 
-bot.on('message', function (user, userID, channelID, message, evt) {
+bot.on('message', message => {
 
     //Some commands have message strings instead of commands.
     if (message.author.bot) return;
 
+    const msg = message.content;
+    const channel = message.channel;
     var d = new Date();
     var weekday = new Array(7);
     weekday[0] = "Sunday";
@@ -51,27 +40,21 @@ bot.on('message', function (user, userID, channelID, message, evt) {
 
     // Our bot needs to know if it will execute a command
     // It will listen for messages that inslude the text `is it ___day`
-    var match = message.search(/is it (mon|tues|wednes|thurs|fri|satur|sun)day/i);
-    var match2 = message.search(/when will it be (mon|tues|wednes|thurs|fri|satur|sun)day/i);
+    var match = msg.search(/is it (mon|tues|wednes|thurs|fri|satur|sun)day/i);
+    var match2 = msg.search(/when will it be (mon|tues|wednes|thurs|fri|satur|sun)day/i);
     if (match >= 0) {
-        var args = message.substring(match+6).split(' ');
+        var args = msg.substring(match+6).split(' ');
         var cmd = args[0].toLowerCase().replace(/\W/g,"");
         cmd = cmd[0].toUpperCase() + cmd.substring(1);
 
         if (day === cmd) {
-            bot.sendMessage({
-                to: channelID,
-                message: "Yes! It's " + cmd + " today! :thumbsup:"
-            });
+            channel.send("Yes! It's " + cmd + " today! :thumbsup:");
         } else {
-            bot.sendMessage({
-                to: channelID,
-                message: "No! It's only " + day + " today. :frowning:"
-            });
+            channel.send("No! It's only " + day + " today. :frowning:");
         }
     }
     if (match2 >= 0) {
-        var args = message.substring(match2+16).split(' ');
+        var args = msg.substring(match2+16).split(' ');
         var cmd = args[0].toLowerCase().replace(/\W/g,"");
         cmd = cmd[0].toUpperCase() + cmd.substring(1);
         var isDay = weekday.indexOf(cmd);
@@ -80,34 +63,24 @@ bot.on('message', function (user, userID, channelID, message, evt) {
             numDay += 7;
         }
         if (numDay === 0) {
-            bot.sendMessage({
-                to: channelID,
-                message: "It's " + cmd + " today! :thumbsup:"
-            });
+            channel.send("It's " + cmd + " today! :thumbsup:");
         } else if (numDay === 1) {
-            bot.sendMessage({
-                to: channelID,
-                message: "It'll be " + cmd + " in " + numDay + " day... :slight_smile:"
-            });
+            channel.send("It'll be " + cmd + " in " + numDay + " day... :slight_smile:");
         } else {
-            bot.sendMessage({
-                to: channelID,
-                message: "It'll be " + cmd + " in " + numDay + " days... :cry:"
-            });
+            channel.send("It'll be " + cmd + " in " + numDay + " days... :cry:");
         }
     }
-    if (message.substring(0,1) == '!') {
-        var args = message.substring(1).split(' ');
+    if (msg.substring(0,1) == '!') {
+        var args = msg.substring(1).split(' ');
         var cmd = args[0];
 
         args = args.splice(1);
         switch(cmd) {
             // !help
             case 'help':
-            bot.sendMessage({
-                to: channelID,
-                message: "The two commands are as follows:\n```\nis it ___day\nwhen will it be ___day\n```"
-            })
+            channel.send("The two commands are as follows:\n```\nis it ___day\nwhen will it be ___day\n```");
         }
     }
 });
+
+bot.login(token);
